8a2d650928886624ae8543665d8c50ad0cba3631
scotex1 <satyampal8303@gmail.com>
2026-07-08 23:01:22 +0300

    Delete hooks/useAuth.ts

--- hooks/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import { useAuthStore } from '@/store/authStore'
-
-export function useAuth() {
-  const state = useAuthStore()
-  return { ...state, firebaseUser: state.uid ? { uid: state.uid } : null }
-}
